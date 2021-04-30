5ae98df2f130a0f88a34418c6f59bcb1cfc4563c
Angel Valera Motos <angelvalera.epal@gmail.com>
2021-04-30 13:29:29 +0200

    Modifico los modelos
    Agrego modelos de mensajes y de chat y modifico el servidor para poder trabajar con sockets

--- models/server.js
+++ models/server.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
+const { createServer } = require("http");
 
 const { dbConnection } = require("../database/config");
+const { socketController } = require("../sockets/controller");
 
 class Server {
 	constructor() {
 		this.app = express();
 		this.port = process.env.PORT || 3000;
-		
+		this.server = createServer(this.app);
+		this.io = require("socket.io")(this.server);
+
 		this.paths = {
 			auth: "/api/auth",
-			buscar:"/api/buscar",		
+			buscar: "/api/buscar",
 			categorias: "/api/categorias",
 			productos: "/api/productos",
 			uploads: "/api/uploads",
@@ -20,15 +24,18 @@ class Server {
 
 		// Conectar a base de datos
 		this.conectarDB();
-		
+
 		// Middlewares
 		this.middlewares();
 
 		// Rutas de mi aplicacion
 		this.routes();
+
+		// Sockets
+		this.sockets();
 	}
 
-	async conectarDB(){
+	async conectarDB() {
 		await dbConnection();
 	}
 
@@ -38,7 +45,7 @@ class Server {
 		this.app.use(this.paths.categorias, require("../routes/categorias"));
 		this.app.use(this.paths.productos, require("../routes/productos"));
 		this.app.use(this.paths.uploads, require("../routes/uploads"));
-        this.app.use( this.paths.usuarios ,require('../routes/usuarios'));
+		this.app.use(this.paths.usuarios, require("../routes/usuarios"));
 	}
 
 	middlewares() {
@@ -51,18 +58,22 @@ class Server {
 		// Directorio Público
 		this.app.use(express.static("public"));
 
-		// Fileupload - Carga de Archivos		
+		// Fileupload - Carga de Archivos
 		this.app.use(
 			fileUpload({
 				useTempFiles: true,
 				tempFileDir: "/tmp/",
-				createParentPath: true
+				createParentPath: true,
 			}),
 		);
 	}
 
+	sockets() {
+		this.io.on("connection", (socket) => socketController(socket, this.io));
+	}
+
 	listen() {
-		this.app.listen(this.port, () => {
+		this.server.listen(this.port, () => {
 			console.log(`Example app listening at http://localhost:${this.port}`);
 		});
 	}
